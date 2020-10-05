71b37eb635ba3e4bad1b13e1fad448b3cab530f1
KiriKaz <igkepsi@gmail.com>
2020-10-05 16:15:25 -0300

    Consistency in column name in order_product table.

--- database/migrations/2020_10_05_170812_create_order_product_table.php
+++ database/migrations/2020_10_05_170812_create_order_product_table.php
@@ -16,7 +16,7 @@ class CreateOrderProductTable extends Migration
         Schema::create('order_product', function (Blueprint $table) {
             $table->id();
             $table->foreignId('order_id')->constrained();
-            $table->foreignId('store_product_id')->constrained('product_store');
+            $table->foreignId('product_store_id')->constrained();
             $table->integer('quantity');
             $table->decimal('amount');
             $table->timestampsTz();
